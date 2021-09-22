a4f86055a8ab5024c91d1bbdc271e26aa0e58922
Eric Chon <echon92@gmail.com>
2021-09-22 16:18:12 -0600

    Complete developer hash model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  'hours_worked_a_day' => 9,
+  'name' => 'Eric',
+  'cohort' => '2110',
+  'hours_of_sleep_lost_a_day' => 3,
+  'resources' => ['google', 'Friends', 'fellow cohort'],
+  'having_fun' => true,
+  'getting_confusing' => true,
+   'new_friends' => ['Wade', 'Kelly', 'Katy']
+}
