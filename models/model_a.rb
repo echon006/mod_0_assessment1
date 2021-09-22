e80430ed203b9d67470964fa98ae95a83d2d1b2c
Eric Chon <echon92@gmail.com>
2021-09-22 16:10:02 -0600

    Complete dino hash model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  'type' => 'T-Rex',
+  'size' => 'large',
+  'miles_traveld_per_day' => 45,
+  'lb_food_eaten_per_day' => 26,
+  'tired' => true,
+  'hungry' => false,
+  'enemies' => ['raptors', 'picking things up', 'water'],
+  'babys_names' => ['joe', 'Moe', 'Shmoe']
+}
