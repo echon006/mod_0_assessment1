4b2c7a6aa12efd0c80c049b745874fdfdd546f6d
Eric Chon <echon92@gmail.com>
2021-09-22 15:54:12 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+movie = "Thor"
+is_entertaining = true
+movie = "Ironman"
+num_4 = 4
+num_6 = 6
+
+product = num_4 * num_6
+p product
+
+favorite_foods = ["Burger", "Fries", "Chicken"]
+favorite_foods.shift
+p favorite_foods
+
+p movie.length
